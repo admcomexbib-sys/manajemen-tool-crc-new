795c26ad2dfcd7177a0ea071d9c4ae8179854b64
admcomexbib-sys <adm.comex.bib@ppa.co.id>
2025-10-24 17:12:17 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD8J9MKV1HmKUUDr-_mJioU9ORFs0Kp2cA",
+  authDomain: "manajemen-tool-crc.firebaseapp.com",
+  databaseURL: "https://manajemen-tool-crc-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "manajemen-tool-crc",
+  storageBucket: "manajemen-tool-crc.firebasestorage.app",
+  messagingSenderId: "856017334993",
+  appId: "1:856017334993:web:78793c395d2e8999184bf0",
+  measurementId: "G-TKS775MDLP"
+};
+const app = firebase.initializeaApp(firebaseConfig);
