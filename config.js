83e4c96e86d0afda7177543042eed883792abf67
admcomexbib-sys <adm.comex.bib@ppa.co.id>
2025-10-24 21:14:05 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,12 +1,10 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD8J9MKV1HmKUUDr-_mJioU9ORFs0Kp2cA",
-  authDomain: "manajemen-tool-crc.firebaseapp.com",
-  databaseURL: "https://manajemen-tool-crc-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "manajemen-tool-crc",
-  storageBucket: "manajemen-tool-crc.firebasestorage.app",
-  messagingSenderId: "856017334993",
-  appId: "1:856017334993:web:78793c395d2e8999184bf0",
-  measurementId: "G-TKS775MDLP"
+  apiKey: "AIzaSyDVkLKGH6Q3ZFtetUufmNdVKZyqQL2M0Mo",
+  authDomain: "sistemmanajementool.firebaseapp.com",
+  projectId: "sistemmanajementool",
+  storageBucket: "sistemmanajementool.firebasestorage.app",
+  messagingSenderId: "1019326909025",
+  appId: "1:1019326909025:web:d5991cd76ec14774367820",
+  measurementId: "G-J9VL1CCZRZ"
 };
-const app = firebase.initializeaApp(firebaseConfig);
